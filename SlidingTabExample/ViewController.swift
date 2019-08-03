5ece26da2c99330e31c8ab76347ca392ae1e2eed
Suprianto D <ersaka96@gmail.com>
2019-08-03 21:54:05 +0800

    Initial Commit

---
+++ SlidingTabExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlidingTabExample
+//
+//  Created by Suprianto Djamalu on 03/08/19.
+//  Copyright © 2019 Suprianto Djamalu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
